a45cd2e1fc962388218cdfec9701c2239eab7e41
pAplakidis <aplakidisp@gmail.com>
2021-01-03 22:58:08 +0200

    fixed scrolling

--- kernel.c
+++ kernel.c
@@ -73,24 +73,12 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y){
   terminal_buffer[index] = vga_entry(c, color);
 }
 
-// TODO: there might be more efficient ways of doing this, O(n^2)
-// TODO: debug this
 void terminal_scroll(){
-  uint16_t temp_buffer[VGA_HEIGHT-1][VGA_WIDTH];
-
-  // Copy terminal contents
-  for(size_t i=1; i<VGA_HEIGHT; i++){
-    for(size_t j=0; j<VGA_WIDTH; j++){
-      const size_t index = i * VGA_WIDTH + j;
-      temp_buffer[i-1][j] = terminal_buffer[index];
-    }
-  }
-
-  // Paste them one line above
-  for(size_t i=1; i<VGA_HEIGHT; i++){
-    for(size_t j=0; j<VGA_WIDTH; j++){
-      const size_t index = (i-1) * VGA_HEIGHT + j;
-      terminal_buffer[index] = temp_buffer[i][j];
+  for(size_t y = 0; y < VGA_HEIGHT-1; y++){
+    for(size_t x = 0; x<VGA_WIDTH; x++){
+      const size_t index1 = y * VGA_WIDTH + x;
+      const size_t index2 = (y+1) * VGA_WIDTH + x;
+      terminal_buffer[index1] = terminal_buffer[index2];
     }
   }
 }
@@ -118,6 +106,7 @@ void terminal_putchar(char c){
   // Scroll temrinal when we reach bottom
   if(terminal_row >= VGA_HEIGHT-1){
     terminal_column = 0;
+    terminal_row--;
     terminal_scroll();
     terminal_putentryat(c, terminal_color, terminal_column, VGA_HEIGHT-1);
   }
@@ -137,17 +126,20 @@ void kernel_main(void){
   // initialize terminal interface
   terminal_initialize();
 
-  terminal_writestring("Hello, kernel World!\nThis is Paul's Operating System\n");
-  terminal_writestring("Hello, kernel World!\nThis is Paul's Operating System\n");
-  terminal_writestring("Hello, kernel World!\nThis is Paul's Operating System\n");
-  terminal_writestring("Hello, kernel World!\nThis is Paul's Operating System\n");
-  terminal_writestring("Hello, kernel World!\nThis is Paul's Operating System\n");
-  terminal_writestring("Trying to\nReach the bottom\n");
-  terminal_writestring("Trying to\nReach the bottom\n");
-  terminal_writestring("Trying to\nReach the bottom\n");
-  terminal_writestring("Trying to\nReach the bottom\n");
-  terminal_writestring("Trying to\nReach the bottom\n");
-  terminal_writestring("Trying to\nReach the bottom\n");
-  //terminal_writestring("Trying to..\nReached it\n");
+  // Test scrolling
+  terminal_writestring("1) Hello, kernel World!\n2) This is Paul's Operating System\n");
+  terminal_writestring("3) Hello, kernel World!\n4) This is Paul's Operating System\n");
+  terminal_writestring("5) Hello, kernel World!\n6) This is Paul's Operating System\n");
+  terminal_writestring("7) Hello, kernel World!\n8) This is Paul's Operating System\n");
+  terminal_writestring("9) Hello, kernel World!\n10) This is Paul's Operating System\n");
+  terminal_writestring("11) Trying to\n12) Reach the bottom\n");
+  terminal_writestring("13) Trying to\n14) Reach the bottom\n");
+  terminal_writestring("14) Trying to\n16) Reach the bottom\n");
+  terminal_writestring("17) Trying to\n18) Reach the bottom\n");
+  terminal_writestring("19) Trying to\n20) Reach the bottom\n");
+  terminal_writestring("21) Trying to\n22) Reach the bottom\n");
+  terminal_writestring("Trying to..\nReached it\n");
+  terminal_writestring("Let's go further down\n");
+  terminal_writestring("Top should be at 4)\n");
 }
 

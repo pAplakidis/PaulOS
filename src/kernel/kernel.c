9e656f7db77abcdb251ecda81fe1abb2c2b60708
pAplakidis <user.email=aplakidisp@gmail.com>
2022-03-11 16:05:04 +0200

    get memory map via GRUB

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -6,21 +6,24 @@
 #include "kernel/kstdio.h"
 #include <kernel/arch/x86/global_descriptor_table.h>
 #include <kernel/arch/x86/interrupts.h>
+#include <kernel/arch/x86/multiboot.h>
 
 // Check if the compiler thinks you are targeting the wrong operating system
-/*
 #if defined(__linux__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#error "This needs to be compiled with a ix86-elf compiler"
 #endif 
-*/
 
 struct GDT_desc gdt_desc;
 
 // TODO: change GDT and IDT from 64bit to 32bit!!!
 
-void kinit(){
+// MAIN
+void kernel_main(multiboot_info_t* mbd, uint32_t magic){
+  // INIT KERNEL
   // initialize terminal interface
   terminal_initialize();
+
+  // ---------------------------------------------------------
 
   //init_GDT();
   gdt_desc.size = sizeof(GDT) -1;
@@ -28,11 +31,42 @@ void kinit(){
   kprintf("GDT size: %d\n", gdt_desc.size);
   kprintf("GDT offset: 0x%x\n", gdt_desc.offset);
   //load_GDT(&gdt_desc);  // TODO: this crashes
-}
+  kprintf("[Kernel]: GDT initialized\n");
 
-// MAIN
-void kernel_main(void){
-  kinit();
+  // ---------------------------------------------------------
+
+  // get memory map via GRUB
+  kprintf("magic: %d\n", magic);
+  // make sure the magic number matches for memory mapping
+  if(magic != MULTIBOOT_BOOTLOADER_MAGIC){
+    // TODO: panic
+    kprintf("[Kernel Panic] Invalid magic number\n");
+  }
+
+  // check bit 6 to see if we have a valid memory map
+  if(!mbd->flags >> 6 & 0x1){
+    kprintf("[Kernel Panic]: Invalid memory map given by GRUB\n");
+  }
+
+  // loop through the memory map and display the values
+  for (int i=0; i < mbd->mmap_length; i += sizeof(multiboot_memory_map_t)){
+    multiboot_memory_map_t* mmmt = (multiboot_memory_map_t*)(mbd->mmap_addr + i);
+    kprintf("Start Addr: 0x%x | ", mmmt->addr);
+    kprintf("Length : 0x%x | ", mmmt->len);
+    kprintf("Size: 0x%x | ", mmmt->size);
+    kprintf("Type: %d\n", mmmt->type);
+
+    if(mmmt->type = MULTIBOOT_MEMORY_AVAILABLE){
+      // TODO
+      /*
+      Do something with this memory block
+      WARNING: some of memory shown as available is actually
+      actively being used by the kernel.
+      */
+    }
+  }
+
+  // ---------------------------------------------------------
 
   // test printing integers on tty
   tputs(uint_to_string((uint32_t)1234567890));
@@ -47,6 +81,7 @@ void kernel_main(void){
   tputs("\n");
 
   // test kprintf
+  /*
   kprintf("Hello World!\n");
   kprintf("This is Mr Kernel\n");
   kprintf("Testing char %c\n", 'a');
@@ -56,5 +91,6 @@ void kernel_main(void){
   kprintf("Testing float %f\n", 3.14);
   kprintf("Testing negative float %f\n", -3.14);
   kprintf("I am number %d, %c!\n", 1, 'A');
+  */
 }
 
